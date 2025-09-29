118f209233eea0e505e0f279175e53d3a689d728
dev-caw <davison.menezes@caw.adv.br>
2025-09-29 09:17:47 -0300

    Corrige nome da classe AppComponent para App em main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-// DENTRO DE: src/main.ts (VERSÃO 100% CORRIGIDA)
+// DENTRO DE: src/main.ts (VERSÃO FINAL E CORRETA)
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { App } from './app/app'; // NOME DA CLASSE CORRIGIDO
+import { App } from './app/app'; // MUDANÇA AQUI
 import { routes } from './app/app.routes';
 
-bootstrapApplication(App, { // NOME DA CLASSE CORRIGIDO
+bootstrapApplication(App, { // E MUDANÇA AQUI
   providers: [
     provideRouter(routes)
   ]
