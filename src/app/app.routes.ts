f4c2d47aaae541e846c44ac407ab7a393976a22d
dev-caw <davison.menezes@caw.adv.br>
2025-09-29 09:08:26 -0300

    Cria a estrutura de componentes dos módulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,27 @@
-// DENTRO DE: src/app/app.routes.ts (VERSÃO FINAL CORRIGIDA)
+// DENTRO DE: src/app/app.routes.ts (VERSÃO FINAL E CORRETA)
 
 import { Routes } from '@angular/router';
 
-// Importe os componentes com os nomes de arquivo corretos
-import { BoasVindasComponent } from './modules/boas-vindas/boas-vindas';
-import { MetodologiaComponent } from './modules/metodologia/metodologia';
-import { EstrategiaLiderancaComponent } from './modules/estrategia-lideranca/estrategia-lideranca';
-import { PessoasCulturaComponent } from './modules/pessoas-cultura/pessoas-cultura';
-import { ProcessosTecnologiaComponent } from './modules/processos-tecnologia/processos-tecnologia';
-import { DiagnosticoComponent } from './modules/diagnostico/diagnostico';
-import { EssencialidadeComponent } from './modules/essencialidade/essencialidade';
-import { CriticidadeComponent } from './modules/criticidade/criticidade';
-import { DesenvolvimentoComponent } from './modules/desenvolvimento/desenvolvimento';
+// Importe as classes com seus nomes reais
+import { BoasVindas } from './modules/boas-vindas/boas-vindas';
+import { Metodologia } from './modules/metodologia/metodologia';
+import { EstrategiaLideranca } from './modules/estrategia-lideranca/estrategia-lideranca';
+import { PessoasCultura } from './modules/pessoas-cultura/pessoas-cultura';
+import { ProcessosTecnologia } from './modules/processos-tecnologia/processos-tecnologia';
+import { Diagnostico } from './modules/diagnostico/diagnostico';
+import { Essencialidade } from './modules/essencialidade/essencialidade';
+import { Criticidade } from './modules/criticidade/criticidade';
+import { Desenvolvimento } from './modules/desenvolvimento/desenvolvimento';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/boas-vindas', pathMatch: 'full' },
-    { path: 'boas-vindas', component: BoasVindasComponent },
-    { path: 'metodologia', component: MetodologiaComponent },
-    { path: 'estrategia-lideranca', component: EstrategiaLiderancaComponent },
-    { path: 'pessoas-cultura', component: PessoasCulturaComponent },
-    { path: 'processos-tecnologia', component: ProcessosTecnologiaComponent },
-    { path: 'diagnostico', component: DiagnosticoComponent },
-    { path: 'essencialidade', component: EssencialidadeComponent },
-    { path: 'criticidade', component: CriticidadeComponent },
-    { path: 'desenvolvimento', component: DesenvolvimentoComponent }
+    { path: 'boas-vindas', component: BoasVindas },
+    { path: 'metodologia', component: Metodologia },
+    { path: 'estrategia-lideranca', component: EstrategiaLideranca },
+    { path: 'pessoas-cultura', component: PessoasCultura },
+    { path: 'processos-tecnologia', component: ProcessosTecnologia },
+    { path: 'diagnostico', component: Diagnostico },
+    { path: 'essencialidade', component: Essencialidade },
+    { path: 'criticidade', component: Criticidade },
+    { path: 'desenvolvimento', component: Desenvolvimento }
 ];
