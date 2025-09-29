c61b8120d59e753795ee88fd0bdce3f94ada1c23
dev-caw <davison.menezes@caw.adv.br>
2025-09-29 10:18:20 -0300

    Nova Versão

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// DENTRO DE: src/app/app.component.ts (ou app.ts)
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.component.html', // (ou app.html)
+  styleUrls: ['./app.component.scss'] // (ou app.scss)
 })
-export class App {
-  protected readonly title = signal('portal-gc-inpi');
+export class App { // ou AppComponent
+  title = 'portal-gc-inpi';
+  isSidebarCollapsed = false; // Controla o estado da sidebar
+
+  toggleSidebar() {
+    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
 }
